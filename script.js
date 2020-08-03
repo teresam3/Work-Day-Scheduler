97da5c970c621593692fef2933cc07765296adb2
chilimango <maldonadot55@yahoo.com>
2020-08-03 16:58:47 -0700

    added minute moment

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var m = moment()
+var sec = m.minute()
 var currentDate = m.format("LL")
 
 //adds current day to header/jumbotron
 $("#currentDay").append(currentDate)
 
 console.log(m.get('hour'))
+console.log(sec)
