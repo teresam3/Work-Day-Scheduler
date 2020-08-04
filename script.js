f9d5e3c80917ff43bf06ba536fb341a39683b32e
chilimango <maldonadot55@yahoo.com>
2020-08-03 20:39:14 -0700

    added the color code function

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-var m = moment()
-var sec = m.minute()
-var currentDate = m.format("LL")
+$(document).ready(function() {
+
+var currentDate = moment().format("LL")
+var blockHour = $(".hour")
+var time
+var currentHour = time
 
 //adds current day to header/jumbotron
 $("#currentDay").append(currentDate)
 
-console.log(m.get('hour'))
-console.log(sec)
+function currentTime() { 
+    var time = moment().format('h:mm a')
+    $("#currentTime").text(time)
+}
+var interval = setInterval(currentTime, 1000)
+
+function eventPlanner() {
+    $("#event").add("form")
+}
+eventPlanner()
+
+for (var i=0; i < blockHour.length; i++) {
+    console.log(blockHour[i].textContent.trim())
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } 
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+}
+
+// function saveMe() {
+
+// }
+
+
+
+
+//console.log(m.get('hour'))
+//console.log(sec)
+
+})
