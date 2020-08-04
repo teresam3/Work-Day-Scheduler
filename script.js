2db54227e05d95dd8d785d82e91c782369354ad5
chilimango <maldonadot55@yahoo.com>
2020-08-03 22:24:11 -0700

    beautify

--- script.js
+++ script.js
@@ -13,43 +13,38 @@ function currentTime() {
     var time = moment().format('h:mm a')
     $("#currentTime").text(time)
 }
-var interval = setInterval(currentTime, 1000)
+    setInterval(currentTime, 1000)
 
 //added input into event id
 function eventPlanner() {
-    $("#event").add("form", "text")
+    $("#event").add("form").val("text")
 }
+
 eventPlanner()
 colorCode()
 
 //changes the color of event id based on time
 function colorCode() {
-for (var i=0; i < blockHour.length; i++) {
-    console.log(blockHour[i].textContent.trim())
-    if (blockHour < currentHour) {
-        $(this).addClass("past");
-      } 
-      else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-      } 
-      else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      }
-}
+    for (var i=0; i < blockHour.length; i++) {
+        console.log(blockHour[i].textContent.trim())
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+        } 
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } 
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    }
 }
 
 //saves to local storage
 // function saveMe() {
 
 // }
-
-
-
-
-//console.log(m.get('hour'))
-//console.log(sec)
 
 })
