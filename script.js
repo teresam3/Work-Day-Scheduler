ebf667e15dd60d95e0dcbed2981c1462bcbe7746
chilimango <maldonadot55@yahoo.com>
2020-08-03 20:46:11 -0700

    added comments

--- script.js
+++ script.js
@@ -8,17 +8,22 @@ var currentHour = time
 //adds current day to header/jumbotron
 $("#currentDay").append(currentDate)
 
+//added ticking time to the jumbotron
 function currentTime() { 
     var time = moment().format('h:mm a')
     $("#currentTime").text(time)
 }
 var interval = setInterval(currentTime, 1000)
 
+//added input into event id
 function eventPlanner() {
-    $("#event").add("form")
+    $("#event").add("form", "text")
 }
 eventPlanner()
+colorCode()
 
+//changes the color of event id based on time
+function colorCode() {
 for (var i=0; i < blockHour.length; i++) {
     console.log(blockHour[i].textContent.trim())
     if (blockHour < currentHour) {
@@ -34,7 +39,9 @@ for (var i=0; i < blockHour.length; i++) {
         $(this).addClass("future");
       }
 }
+}
 
+//saves to local storage
 // function saveMe() {
 
 // }
